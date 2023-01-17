6661b47b8d2b7594363e1d56900b02f17b116d1b
Toshimichi0915 <main@toshimichi.net>
2023-01-18 00:21:00 +0900

    Initial commit

---
+++ src/main/java/net/toshimichi/mcbase/Main.java
@@ -0,0 +1,11 @@
+package net.toshimichi.mcbase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello World!");
+    }
+}
